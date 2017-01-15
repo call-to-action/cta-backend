dff5f5e04cab7d25e5971f6b8fccdd05c2372173
Meredith Finkelstein <meredith@printallover.me>
2017-01-15 11:12:09 -0500

    has stubs for all api actions  - untested

--- cta/views.py
+++ cta/views.py
@@ -1,5 +1,5 @@
 from .models import *
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .serializers import  *
 
 
@@ -7,20 +7,128 @@ class CtaUserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = CtaUser.objects.all().order_by('-date_joined')
+    queryset = CtaUser.objects.all().order_by('-created_at')
     serializer_class = CtaUserPublicSerializer
 
 
 class CallToActionViewSet(viewsets.ModelViewSet):
-    queryset = CtaUser.objects.all().order_by('-date_joined')
+    queryset = CtaUser.objects.all().order_by('-created_at')
     serializer_class = CallToActionSerializer
 
 class UserCallToActionViewSet(viewsets.ModelViewSet):
-    queryset = UserCallToAction.objects.all().order_by('-date_joined')
+    queryset = UserCallToAction.objects.all().order_by('-created_at')
     serializer_class = UserCallToActionSerializer
 
 
 class UserSubscriptionViewSet(viewsets.ModelViewSet):
-    queryset = UserSubscription.objects.all().order_by('-date_joined')
+    queryset = UserSubscription.objects.all().order_by('-created_at')
     serializer_class = UserSubscriptionSerializer
 
+
+class GetUserCtas(generics.ListCreateAPIView):
+    queryset = CallToAction.objects.all()
+    serializer_class = CallToActionSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(created_by=self.request.user)
+        serializer = CallToActionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class GetBookmarkedCtas(generics.ListCreateAPIView):
+    queryset = UserCallToAction.objects.all()
+    serializer_class = UserCallToActionSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(user=self.request.user,status=2) #bogus status
+        serializer = CallToActionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class GetApprovedCtas(generics.ListCreateAPIView):
+    queryset = UserCallToAction.objects.all()
+    serializer_class = UserCallToActionSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(user=self.request.user,status=1) #bogus status
+        serializer = CallToActionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class GetDeclinedCtas(generics.ListCreateAPIView):
+    queryset = UserCallToAction.objects.all()
+    serializer_class = UserCallToActionSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(user=self.request.user,status=0) #bogus status
+        serializer = CallToActionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
+class GetAvailableCtas(generics.ListCreateAPIView):
+    queryset = CallToAction.objects.all()
+    serializer_class = CallToActionSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(created_by=self.request.user) #NOT IN CAllToActionTable
+        serializer = CallToActionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class SearchAvailableCtas(generics.ListCreateAPIView):
+    queryset = CallToAction.objects.all()
+    serializer_class = CallToActionSerializer
+
+    def list(self, request):
+        term = self.kwargs['term']
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(tag__contains=term) #NOT IN CAllToActionTable
+        serializer = CallToActionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class GetLoggedInUser(generics.ListCreateAPIView):
+    queryset = CtaUser.objects.all()
+    serializer_class = CtaUserPrivateSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(user=self.request.user)
+        serializer = CtaUserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
+
+class GetSubscribers(generics.ListCreateAPIView):
+    queryset = UserSubscription.objects.all()
+    serializer_class = UserSubscriptionSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(subscription=self.request.user)
+        serializer = UserSubscriptionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class GetSubscriptions(generics.ListCreateAPIView):
+    queryset = UserSubscription.objects.all()
+    serializer_class = UserSubscriptionSerializer
+
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        queryset = queryset.filter(subscribers=self.request.user)
+        serializer = UserSubscriptionSerializer(queryset, many=True)
+        return Response(serializer.data)
